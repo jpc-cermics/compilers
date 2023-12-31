84fc7bea3b68ac90bc6b78708c34f72600275eb0
Jean-Philippe Chancelier <jpc@cermics.enpc.fr>
2023-12-31 13:06:58 +0100

    updates

--- modelicac/src/graphNodeSet.mli
+++ modelicac/src/graphNodeSet.mli
@@ -31,19 +31,19 @@ module type HashableType =
     type t
     val hash: t -> int
   end
-    (** A hashable type is a type provided with one function whose purpose is to
+    (* A hashable type is a type provided with one function whose purpose is to
        get an integer value that represents the hash code of objects, i.e. an
        integer associated. *)
 
 module type S =
   sig
     type elt
-    (** The type of the elements in the weak collection. *)
+    (* The type of the elements in the weak collection. *)
     type 'a t
-    (** The type of the weak collection. *)
+    (* The type of the weak collection. *)
     val create:
       int -> ('a -> elt -> bool) -> ('a -> int) -> ('a -> int -> elt) -> 'a t
-    (** The parameters of create are:
+    (* The parameters of create are:
            - the initial size of the collection;
            - an equality function that does does create a new object but rather
              uses some atoms;
@@ -52,11 +52,11 @@ module type S =
            - a constructor that creates a new object, given the necessary atoms
              plus a hash value. *)
     val find_or_add: 'a -> 'a t -> elt
-    (** find_or_add either returns an existing object whose atoms are the same
+    (* find_or_add either returns an existing object whose atoms are the same
          as the ones given as the first argument, either builds a new object and
          insert it into the collection before returning it. *)
     val iter: (elt -> unit) -> 'a t -> unit
-    (** iter applies its first argument to every argument of the collection. *)
+    (* iter applies its first argument to every argument of the collection. *)
   end
 
 module Make:
@@ -71,6 +71,6 @@ module Make:
     end
 
 val hash_param: int -> int -> 'a -> int
-(** See {!Hashtbl.hash_param}. *)
+(* See {!Hashtbl.hash_param}. *)
 val hash: 'a -> int
-(** See {!Hashtbl.hash}. *)
+(* See {!Hashtbl.hash}. *)
